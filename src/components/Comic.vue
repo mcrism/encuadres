235a8dace98a26fd31dfb492e077c8e91b01b0cf
Cristina <mcristina.marcosm@gmail.com>
2022-06-06 21:26:08 +0200

    Estilos de vinetas

--- src/components/Comic.vue
+++ src/components/Comic.vue
@@ -1,4 +1,10 @@
 <script lang="ts">
+
+import Vue from 'vue'
+
+// @ts-ignore
+//import VueVirtualScroller from 'vue-virtual-scroller'
+
 export default {
   data () {
     return {
@@ -6,36 +12,43 @@ export default {
       controlPrimeraCamara: true,
     };
   },
+  components: {
+    //VueVirtualScroller,
+  },
   methods: {
+    // vineta5: function() {
+    //   return '../assets/Vinetas/5_2.gif?a=${Math.random()}';
+    // }
   },
   computed: {
-    random5(): string {
-      return "../assets/Vinetas/5_2.gif";
-    }
+    // random5(): string {
+    //   return "../assets/Vinetas/5_2.gif";
+    // }
   }
 };
 </script>
 
 <template>
   <div class="vertical">
+    
     <img alt="1" class="vineta-estandar" src="../assets/Vinetas/1_Vineta1.png" />
     <img alt="2" class="vineta-solape-superior" src="../assets/Vinetas/2_Vineta2.png" />
-    <img alt="3" class="vineta-estandar" src="../assets/Vinetas/3_Vineta3.gif" />
-    <img alt="4" class="vineta-estandar" src="../assets/Vinetas/4_Vineta4.png" />
+    <img alt="3" class="vineta-media" src="../assets/Vinetas/3_Vineta3.gif" />
+    <img alt="4" class="vineta-media" src="../assets/Vinetas/4_Vineta4.png" />
     <div class="vineta-vacia" v-if="control_vineta5" @click="control_vineta5=!control_vineta5">
       <img alt="5_1" class="vineta-estandar" src="../assets/Vinetas/5_1.gif" />
     </div>
     <div v-else class="vineta-vacia" @click="control_vineta5=!control_vineta5">
-      <img id="gif-5-2" alt="5_2" class="vineta-estandar" src="../assets/Vinetas/5_2.gif" @click="document.getElementById('gif-5-2').src='../assets/Vinetas/5_2.gif' + '?a='+Math.random()"/>
+      <img id="gif-5-2" alt="5_2" class="vineta-estandar" src="../assets/Vinetas/5_2.gif">
     </div>
     <img alt="6" class="vineta-estandar" src="../assets/Vinetas/6_Vineta6.png" />
     <img alt="7" class="vineta-estandar" src="../assets/Vinetas/7_Vineta7.png" />
-    <img alt="8" class="vineta-solape-superior" src="../assets/Vinetas/8_Vineta8.png" />
+    <img alt="8" class="vineta-estandar" src="../assets/Vinetas/8_Vineta8.png" />
     <img alt="9" class="vineta-estandar" src="../assets/Vinetas/9_Vineta9.gif" />
     <img alt="10" class="vineta-estandar" src="../assets/Vinetas/10_Vineta10.gif" />
-    <img alt="11" class="vineta" src="../assets/Vinetas/11_Vineta11.gif" />
-    <img alt="12" class="vineta" src="../assets/Vinetas/12.png" />
-    <img alt="6" class="vineta" src="../assets/Vinetas/15.gif" />
+    <img alt="11" class="vineta-estandar" src="../assets/Vinetas/11_Vineta11.gif" />
+    <img alt="12" class="vineta-estandar" src="../assets/Vinetas/12.png" />
+    <img alt="6" class="vineta-estandar" src="../assets/Vinetas/15.gif" />
     <div class="horizontales">
       <img alt="16" class="vineta-media" src="../assets/Vinetas/16_2.png" />
       <div v-if="controlPrimeraCamara" @click="cambioControlPrimeraCamara">
@@ -77,6 +90,7 @@ export default {
 
 .vineta-estandar {
   max-width: 1366px;
+  width: 100%;
 }
 
 .vineta-solape-superior {
@@ -87,10 +101,12 @@ export default {
 .vineta-vacia {
   height: 1024px;
   min-height: 1024px;
+  width: 100%;;
 }
 
 .vineta-media {
-  width: 50%;
+  height: 50%;
+  width: 100%;
 }
 
 .horizontales {
