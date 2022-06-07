4b24c18f91a8eb83dcaee861d954fd2af848bb2a
Cristina <mcristina.marcosm@gmail.com>
2022-06-07 18:27:29 +0200

    Carga optimizada de imágenes

--- src/components/Comic.vue
+++ src/components/Comic.vue
@@ -1,25 +1,23 @@
 <script lang="ts">
-
-import { defineComponent } from 'vue';
-
-//import VueVirtualScroller from 'vue-virtual-scroller'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Comic',
-  data () {
+  name: "Comic",
+  components: {},
+  data() {
     return {
       control_vineta5: true,
       controlPrimeraCamara: true,
-    }
+    };
   },
   methods: {
     vineta5() {
-      return '../assets/Vinetas/5_2.gif?a=${Math.random()}';
+      return "../assets/Vinetas/5_2.gif?a=${Math.random()}";
     },
 
     restartGIF(id: string, url: string) {
       console.log("El id que obtengo es ", id);
-      const divImg = document.getElementById('vineta1');
+      const divImg = document.getElementById("vineta1");
       console.log("El elemento que obtengo del documento es ", divImg);
       const prueba = !this.control_vineta5;
       if (divImg) {
@@ -32,47 +30,123 @@ export default defineComponent({
 
     mostrarVineta5(): void {
       this.control_vineta5 = !this.control_vineta5;
-      this.restartGIF('gif52', "../assets/Vinetas/5_2.gif");
-    }
-
-  },
-  components: {
-    //VueVirtualScroller,
+      this.restartGIF("gif52", "../assets/Vinetas/5_2.gif");
+    },
   },
   computed: {
     random5(): string {
       return "../assets/Vinetas/5_2.gif";
     },
-  }
+  },
 });
 </script>
 
 
 <template>
   <div class="vertical">
-    
-    <img id="vineta1" alt="1" class="vineta-estandar" src="../assets/Vinetas/1_Vineta1.png" />
-    <img alt="2" class="vineta-solape-superior" src="../assets/Vinetas/2_Vineta2.png" />
-    <img alt="3" class="vineta-media" src="../assets/Vinetas/3_Vineta3.gif" />
-    <img alt="4" class="vineta-media" src="../assets/Vinetas/4_Vineta4.png" />
-    <div class="vineta-vacia" v-if="control_vineta5" @click="control_vineta5=!control_vineta5" >
-      <img alt="5_1" class="vineta-estandar" src="../assets/Vinetas/5_1.gif" />
+    <img
+      loading="lazy"
+      id="vineta1"
+      alt="1"
+      class="vineta-estandar"
+      src="../assets/Vinetas/1_Vineta1.png"
+    />
+    <img
+      loading="lazy"
+      alt="2"
+      class="vineta-solape-superior"
+      src="../assets/Vinetas/2_Vineta2.png"
+    />
+    <img
+      loading="lazy"
+      alt="3"
+      class="vineta-media"
+      src="../assets/Vinetas/3_Vineta3.gif"
+    />
+    <img
+      loading="lazy"
+      alt="4"
+      class="vineta-media"
+      src="../assets/Vinetas/4_Vineta4.png"
+    />
+    <div
+      class="vineta-vacia"
+      v-if="control_vineta5"
+      @click="control_vineta5 = !control_vineta5"
+    >
+      <img
+        loading="lazy"
+        alt="5_1"
+        class="vineta-estandar"
+        src="../assets/Vinetas/5_1.gif"
+      />
     </div>
     <div v-else class="vineta-vacia" @click="mostrarVineta5()">
-      <img alt="5_2" class="vineta-estandar" src="../assets/Vinetas/5_2.gif">
+      <img
+        loading="lazy"
+        alt="5_2"
+        class="vineta-estandar"
+        src="../assets/Vinetas/5_2.gif"
+      />
     </div>
-    <img alt="6" class="vineta-estandar" src="../assets/Vinetas/6_Vineta6.png" />
-    <img alt="7" class="vineta-estandar" src="../assets/Vinetas/7_Vineta7.png" />
-    <img alt="8" class="vineta-estandar" src="../assets/Vinetas/8_Vineta8.png" />
-    <img alt="9" class="vineta-estandar" src="../assets/Vinetas/9_Vineta9.gif" />
-    <img alt="10" class="vineta-estandar" src="../assets/Vinetas/10_Vineta10.gif" />
-    <img alt="11" class="vineta-estandar" src="../assets/Vinetas/11_Vineta11.gif" />
-    <img alt="12" class="vineta-estandar" src="../assets/Vinetas/12.png" />
-    <img alt="6" class="vineta-estandar" src="../assets/Vinetas/15.gif" />
+    <img
+      loading="lazy"
+      alt="6"
+      class="vineta-estandar"
+      src="../assets/Vinetas/6_Vineta6.png"
+    />
+    <img
+      loading="lazy"
+      alt="7"
+      class="vineta-estandar"
+      src="../assets/Vinetas/7_Vineta7.png"
+    />
+    <img
+      loading="lazy"
+      alt="8"
+      class="vineta-estandar"
+      src="../assets/Vinetas/8_Vineta8.png"
+    />
+    <img
+      loading="lazy"
+      alt="9"
+      class="vineta-estandar"
+      src="../assets/Vinetas/9_Vineta9.gif"
+    />
+    <img
+      loading="lazy"
+      alt="10"
+      class="vineta-estandar"
+      src="../assets/Vinetas/10_Vineta10.gif"
+    />
+    <img
+      loading="lazy"
+      alt="11"
+      class="vineta-estandar"
+      src="../assets/Vinetas/11_Vineta11.gif"
+    />
+    <img
+      loading="lazy"
+      alt="12"
+      class="vineta-estandar"
+      src="../assets/Vinetas/12.png"
+    />
+    <img
+      loading="lazy"
+      alt="6"
+      class="vineta-estandar"
+      src="../assets/Vinetas/15.gif"
+    />
     <div class="horizontales">
-      <img alt="16" class="vineta-media" src="../assets/Vinetas/16_2.png" />
+      <img
+        loading="lazy"
+        alt="16"
+        class="vineta-media"
+        src="../assets/Vinetas/16_2.png"
+      />
       <div v-if="controlPrimeraCamara">
         <img
+          loading="lazy"
           alt="16"
           class="vineta-media-vacia"
           src="../assets/Vinetas/16_1.png"
@@ -80,28 +154,43 @@ export default defineComponent({
       </div>
       <div v-else>
         <img
+          loading="lazy"
           alt="16"
           class="vineta-media-vacia"
           src="../assets/Vinetas/16.gif"
         />
       </div>
     </div>
-    <img alt="13" class="vineta" src="../assets/Vinetas/13.png" />
-    <!-- <img alt="6" class="vineta" src="../assets/Vinetas/.png" />
-    <img alt="6" class="vineta" src="../assets/Vinetas/.png" /> -->
-    <img alt="14" class="vineta" src="../assets/Vinetas/14.png" />
-    <img alt="21" class="vineta-estandar" src="../assets/Vinetas/21_Vineta21.gif" />
+    <img
+      loading="lazy"
+      alt="13"
+      class="vineta"
+      src="../assets/Vinetas/13.png"
+    />
+    <img loading="lazy" alt="6" class="vineta" src="../assets/Vinetas/.png" />
+    <img loading="lazy" alt="6" class="vineta" src="../assets/Vinetas/.png" />
+    <img
+      loading="lazy"
+      alt="14"
+      class="vineta"
+      src="../assets/Vinetas/14.png"
+    />
+    <img
+      loading="lazy"
+      alt="21"
+      class="vineta-estandar"
+      src="../assets/Vinetas/21_Vineta21.gif"
+    />
   </div>
 </template>
 
 <style scoped>
-
-
 .vertical {
   display: flex;
   flex-direction: column;
   background-color: var(--color-yellow);
-  background-image: radial-gradient(var(--color-dark-yellow) 20%, transparent 0), radial-gradient(var(--color-dark-yellow) 20%, transparent 0);
+  background-image: radial-gradient(var(--color-dark-yellow) 20%, transparent 0),
+    radial-gradient(var(--color-dark-yellow) 20%, transparent 0);
   background-size: 20px 20px;
   background-position: 0 0, 10px 10px;
   background-color: white;
@@ -121,7 +210,7 @@ export default defineComponent({
 .vineta-vacia {
   height: 1024px;
   min-height: 1024px;
-  width: 100%;;
+  width: 100%;
 }
 
 .vineta-media {
